ec7d8e52f0de2f4bca062c118aceae0c17be019d
SantiagoLazarec <santiagolazarec014@gmail.com>
2022-02-08 22:22:08 -0300

    Desafio1

---
+++ script.js
@@ -0,0 +1,45 @@
+let val = NaN
+
+const suma = (num1, num2, num3) => num1 + num2 + num3 
+const suma1 = (num1, num2, num3) => num1 + num2 - num3 
+const suma2 = (num1, num2, num3) => num1 + num2 * num3 
+const suma3 = (num1, num2, num3) => num1 + num2 / num3 
+
+const resta = (num1, num2, num3) => num1 - num2 - num3 
+const resta1 = (num1, num2, num3) => num1 - num2 + num3
+const resta2 = (num1, num2, num3) => num1 - num2 * num3
+const resta3 = (num1, num2, num3) => num1 - num2 / num3
+
+const multiplicacion = (num1, num2, num3) => num1 * num2 * num3
+const multiplicacion1 = (num1, num2, num3) => num1 * num2 + num3
+const multiplicacion2 = (num1, num2, num3) => num1 * num2 - num3
+const multiplicacion3 = (num1, num2, num3) => num1 * num2 / num3
+
+const division = (num1, num2, num3) => num1 / num2 / num3 
+const division1 = (num1, num2, num3) => num1 / num2 + num3 
+const division2 = (num1, num2, num3) => num1 / num2 - num3 
+const division3 = (num1, num2, num3) => num1 / num2 * num3 
+
+console.log(suma(10,5,7))
+console.log(suma1(10,5,7)) 
+console.log(suma2(10,5,7))
+console.log(suma3(10,5,7)) 
+
+console.log(resta(10,5,7))
+console.log(resta1(10,5,7))
+console.log(resta2(10,5,7))
+console.log(resta3(10,5,7)) 
+
+
+console.log(multiplicacion(10,5,7)) 
+console.log(multiplicacion1(10,5,7))
+console.log(multiplicacion2(10,5,7))
+console.log(multiplicacion3(10,5,7)) 
+
+console.log(division(10,5,7))
+console.log(division1(10,5,7))
+console.log(division2(10,5,7))
+console.log(division3(10,5,7))
+
+
+
